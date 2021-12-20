450c3b317039330388da76d112c732787ed63303
Tranvansu2001 <Tranvansu2001@gmail.com>
2021-12-20 09:42:32 +0700

    redux demo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/cake/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <HooksCakeContainer />
+        <CakeContainer />
+        <IceCreamContainer />
+        <NewCakeContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
